d4accdabe5ef17ed621dc87f37010cce8c8ccb9b
Rahul Sinha <rahulsinha1996@gmail.com>
2022-07-20 19:09:50 +0530

    day2

---
+++ jio/src/components/Product/MainProduct.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Product } from './Product'
+
+export const MainProduct = () => {
+  return (
+    <Product/>
+  )
+}
---
+++ jio/src/components/Product/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Product.module.css'
+
+export const Product = () => {
+  return (
+    <div className={styles.product}>
+        <p>Name</p>
+        <p>Price</p>
+    </div>
+  )
+}
