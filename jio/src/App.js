c416ebf903c4a9a071d8750a6c93ecc9e68c365a
Priyanshu Anany <ananypriyanshu@gmail.com>
2022-07-21 11:41:47 +0530

    c

--- jio/src/App.js
+++ jio/src/App.js
@@ -1,7 +1,7 @@
 
 import React,{useState} from 'react';
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Login from './components/Login/Login';
