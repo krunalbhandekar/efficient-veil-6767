b5058a06ecc619c5aed705d4b2e97529e9c8bb89
Priyanshu Anany <ananypriyanshu@gmail.com>
2022-07-21 18:42:04 +0530

    c

--- jio/src/App.js
+++ jio/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
-// import Login from './components/Login/Login';
-// import Signup from "./components/Login/Signup"
+import Login from './components/Login/Login';
+import Signup from "./components/Login/Signup"
 import Cart from './components/Cart/Cart';
-// import Account from './components/Login/Account';
+import Account from './components/Login/Account';
 import Landingpage from './components/Landingpage/Landingpage';
 import SideNavbar from './components/sideNavbar/SideNavbar';
+import Beauty from './components/Beauty/Beauty';
 
 
 function App() {
@@ -21,10 +22,11 @@ function App() {
     </div>
     <Routes>
        <Route path="/" element={<Landingpage />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/signup" element={<Signup />} /> */}
-        {/* <Route path="/account" element={<Account />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/beauty" element={<Beauty/>}/>
     </Routes>
       <Footer/>
     </>
